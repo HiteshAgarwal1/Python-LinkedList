1604a952d69e83d0dc5dc1cb1905116a37d54b04
Hitesh Agarwal <hiteshagarwal135h@gmail.com>
2020-06-14 14:26:55 +0530

    Update READ.md
    Add the download url and update the classifier with package topic
    and intended audience.

--- setup.py
+++ setup.py
@@ -7,22 +7,31 @@ def readme():
 
 
 setup(
-    name="pylinkedlist",
-    version="0.0.1",
-    description="A Python package to work with LinkedList.",
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    url="",
-    author="Hitesh Agarwal",
-    author_email="contact@hiteshagarwal.com",
-    license="MIT",
-    classifiers=[
+    name = "pylinkedlist",
+    version = "0.0.1",
+    description = "A Python package to work with LinkedList.",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/HiteshAgarwal1/Python-LinkedList",
+    download_url = "https://github.com/HiteshAgarwal1/Python-LinkedList/archive/0.0.1.tar.gz"
+    author = "Hitesh Agarwal",
+    author_email = "contact@hiteshagarwal.com",
+    license = "MIT",
+    keywords = ["linked list", "list", "python linked list"],
+    classifiers = [
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        'Topic :: Linked List',
+        'Topic :: Data Sturucture'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
     ],
-    py_modules=["pylinkedlist"],
-    package_dir={'': 'src'},
-    include_package_data=True,
-    install_requires=[],
+    py_modules = ["pylinkedlist"],
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    install_requires = [],
 )
