25491c65cf74bacf3c30475002ec8956d505113f
Hitesh Agarwal <hiteshagarwal135h@gmail.com>
2020-06-14 23:13:37 +0530

    Update version attr and resolve syntax error of commas

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def readme():
 
 setup(
     name = "pylinkedlist",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "A Python package to work with LinkedList.",
     long_description = readme(),
     long_description_content_type = "text/markdown",
     url = "https://github.com/HiteshAgarwal1/Python-LinkedList",
-    download_url = "https://github.com/HiteshAgarwal1/Python-LinkedList/archive/0.0.1.tar.gz"
+    download_url = "https://github.com/HiteshAgarwal1/Python-LinkedList/archive/0.0.1.tar.gz",
     author = "Hitesh Agarwal",
     author_email = "contact@hiteshagarwal.com",
     license = "MIT",
@@ -23,8 +23,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
-        'Topic :: Linked List',
-        'Topic :: Data Sturucture'
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',
